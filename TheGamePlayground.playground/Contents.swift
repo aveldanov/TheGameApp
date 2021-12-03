f923c38b93b7f614d160b06428145edd18ae67c5
Anton Veldanov <anton.veldanov@gmail.com>
2021-12-02 22:22:55 -0800

    iOS

--- TheGamePlayground.playground/Contents.swift
+++ TheGamePlayground.playground/Contents.swift
@@ -1,3 +1,47 @@
-import UIKit
 
-var greeting = "Hello, playground"
+
+
+var pattern = [1,2,3,4]
+
+var input = [4,2,2,1]
+
+var result = 0
+
+//let pmap = pattern.map{($0,1)}
+//
+//print(pmap)
+//
+//let patternDict = Dictionary(pmap, uniquingKeysWith: +) // [3:1, 4:3]
+//
+//
+//print(patternDict)
+
+
+var dictPatter = [Int:Int]()
+for i in pattern{
+    dictPatter[i] = (dictPatter[i] ?? 0) + 1
+}
+
+//
+var dictInput = [Int:Int]()
+for i in input{
+    dictInput[i] = (dictInput[i] ?? 0) + 1
+}
+
+print(dictPatter, dictInput)
+
+
+
+for key in dictPatter.keys{
+
+    if dictInput[key] != nil{
+        result += min(dictInput[key]!,dictPatter[key]!)
+    }
+}
+
+
+print(result)
+
+var zipped = Array(zip(pattern,input)).filter{$0.0 == $0.1}.count
+
+print(zipped)

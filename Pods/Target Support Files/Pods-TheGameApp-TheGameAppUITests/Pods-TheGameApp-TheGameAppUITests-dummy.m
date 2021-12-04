208a68642ed35706bff4ad25ae13b65c0a90bd16
Anton Veldanov <anton.veldanov@gmail.com>
2021-12-03 19:39:00 -0800

    iOS

---
+++ Pods/Target Support Files/Pods-TheGameApp-TheGameAppUITests/Pods-TheGameApp-TheGameAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TheGameApp_TheGameAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TheGameApp_TheGameAppUITests
+@end
---
+++ Pods/Target Support Files/Pods-TheGameAppTests/Pods-TheGameAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TheGameAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_TheGameAppTests
+@end

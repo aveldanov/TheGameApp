90a7fcdb2eb53818a85a480f6a34a28fad3e4c5f
Anton Veldanov <anton.veldanov@gmail.com>
2021-12-05 23:24:36 -0800

    iOS

--- TheGameApp/Views/LineTableViewCell.swift
+++ TheGameApp/Views/LineTableViewCell.swift
@@ -13,35 +13,24 @@ import UIKit
 //}
 
 class LineTableViewCell: UITableViewCell, SettingsViewControllerDelegate {
-
+    
     private var imageSet = [UIImage]()
-
-//   private let imageSet = [ #imageLiteral(resourceName: "0"), #imageLiteral(resourceName: "1"), #imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "3"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "6"), #imageLiteral(resourceName: "7"), #imageLiteral(resourceName: "dash")]
-    
-//   private let imageSet1 = [ #imageLiteral(resourceName: "orange"), #imageLiteral(resourceName: "black"), #imageLiteral(resourceName: "white"), #imageLiteral(resourceName: "purple"), #imageLiteral(resourceName: "green"), #imageLiteral(resourceName: "blue"), #imageLiteral(resourceName: "yellow"), #imageLiteral(resourceName: "red"), #imageLiteral(resourceName: "dash")]
-    
-    
-    let checkLabels = ["⚪️","⚫️","⭕️"]
+    //    let checkLabels = ["⚪️","⚫️","⭕️"]
     var settingsVC = SettingsViewController()
-
-
+    
     @IBOutlet var imageLineCollection: [UIImageView]!
     @IBOutlet weak var checkButtonOutlet: UIButton!
-    
     @IBOutlet var checkLabelCollection: [UILabel]!
     
     
-//    var delegate: LineTableViewCellProtocol?
+    //    var delegate: LineTableViewCellProtocol?
     
     var viewModel: LineViewModel?{
         didSet{
-//            print("BOOOOOOOOm")
+            //            print("BOOOOOOOOm")
             configure()
         }
     }
-    
-    
-    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -50,14 +39,13 @@ class LineTableViewCell: UITableViewCell, SettingsViewControllerDelegate {
         settingsVC.delegate = self
         settingsVC.toggleStateShared()
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-
+    
     
     func toggleStateData(_ index: Int) {
-        
         if index == 0{
             imageSet = [ #imageLiteral(resourceName: "0"), #imageLiteral(resourceName: "1"), #imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "3"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "6"), #imageLiteral(resourceName: "7"), #imageLiteral(resourceName: "dash")]
         }else{
@@ -65,11 +53,8 @@ class LineTableViewCell: UITableViewCell, SettingsViewControllerDelegate {
         }
     }
     
-    
-    
     func configure(){
-//        delegate?.refreshViewModel()
-
+        //        delegate?.refreshViewModel()
         guard let viewModel = viewModel else{
             return
         }
@@ -80,21 +65,14 @@ class LineTableViewCell: UITableViewCell, SettingsViewControllerDelegate {
         imageLineCollection[1].image = imageSet[viewModel.lines[row].arr[1]]
         imageLineCollection[2].image = imageSet[viewModel.lines[row].arr[2]]
         imageLineCollection[3].image = imageSet[viewModel.lines[row].arr[3]]
-
+        
         checkLabelCollection[0].text = viewModel.lines[row].verifyArr[0]
         checkLabelCollection[1].text = viewModel.lines[row].verifyArr[1]
         checkLabelCollection[2].text = viewModel.lines[row].verifyArr[2]
         checkLabelCollection[3].text = viewModel.lines[row].verifyArr[3]
-        
-//        checkButtonOutlet.isHidden = viewModel.isButtonCheck
     }
     
-    
- 
     @IBAction func checkButtonTapped(_ sender: UIButton) {
-//        checkButtonOutlet.isHidden = true
+        //        checkButtonOutlet.isHidden = true
     }
-    
-    
-    
 }
